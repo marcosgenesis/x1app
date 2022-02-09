0f03b53577aa0df51feff483dd3ed5bbdc76ec92
Marcos Gênesis da Silva <marcosgenesisof@gmail.com>
2022-02-09 17:19:28 -0300

    fix: fake users

--- app/src/main/java/com/example/x1um/Model/Mock/Users.kt
+++ app/src/main/java/com/example/x1um/Model/Mock/Users.kt
@@ -7,24 +7,24 @@ import java.util.*
 object Users {
     fun fakeUsers(): List<User> {
         return Arrays.asList(
-            User.UserBuilder.builder().setId("123").setName("Marcos Nascimento").setEmail("marquin@nascimento.com").setUsername("marquin").build(),
-            User.UserBuilder.builder().setId("1234").setName("Marcos Nogueira").setEmail("marquin@nascimento.com").setUsername("italo").build(),
-            User.UserBuilder.builder().setId("1235").setName("Marcos Mito").setEmail("marquin@nascimento.com").setUsername("nascimento").build(),
-            User.UserBuilder.builder().setId("1236").setName("Marcos Pedra branca").setEmail("marquin@nascimento.com").setUsername("gusta").build(),
-            User.UserBuilder.builder().setId("1237").setName("Marquinhos de morada nova").setEmail("marquin@nascimento.com").setUsername("jefo").build(),
-            User.UserBuilder.builder().setId("1238").setName("Marquin rei delas").setEmail("marquin@nascimento.com").setUsername("ericALenda").build(),
+            User.UserBuilder.builder().setId("123").setName("Marcus").setEmail("marquin@nascimento.com").setUsername("marquin").build(),
+            User.UserBuilder.builder().setId("1234").setName("Eric Rodrigues").setEmail("marquin@nascimento.com").setUsername("italo").build(),
+            User.UserBuilder.builder().setId("1235").setName("Lucas Nascimento").setEmail("marquin@nascimento.com").setUsername("nascimento").build(),
+            User.UserBuilder.builder().setId("1236").setName("Gustabo").setEmail("marquin@nascimento.com").setUsername("gusta").build(),
+            User.UserBuilder.builder().setId("1237").setName("Jefo de morada nova").setEmail("marquin@nascimento.com").setUsername("jefo").build(),
+            User.UserBuilder.builder().setId("1238").setName("Eric rei delas").setEmail("marquin@nascimento.com").setUsername("ericALenda").build(),
 
             )
     }
 
     fun fakeRankingUsers(): List<User> {
         return Arrays.asList(
-            User.UserBuilder.builder().setId("1234").setName("Ítalo Lima").setEmail("italolima@gmail.com").setUsername("italo").build(),
-            User.UserBuilder.builder().setId("12344").setName("Ítalo Mito").setEmail("italo@nascimento.com").setUsername("italo2").build(),
-            User.UserBuilder.builder().setId("12354").setName("Ítalo Rei").setEmail("italo@nascimento.com").setUsername("italo3").build(),
-            User.UserBuilder.builder().setId("12364").setName("Ítalo Pedra branca").setEmail("italo@nascimento.com").setUsername("italo4").build(),
-            User.UserBuilder.builder().setId("12374").setName("Ítalo de morada nova").setEmail("italo@nascimento.com").setUsername("italo5").build(),
-            User.UserBuilder.builder().setId("12384").setName("Ítalo rei delas").setEmail("italo@nascimento.com").setUsername("italos6").build(),
+            User.UserBuilder.builder().setId("123").setName("Marcus").setEmail("marquin@nascimento.com").setUsername("marquin").build(),
+            User.UserBuilder.builder().setId("1234").setName("Eric Rodrigues").setEmail("marquin@nascimento.com").setUsername("italo").build(),
+            User.UserBuilder.builder().setId("1235").setName("Lucas Nascimento").setEmail("marquin@nascimento.com").setUsername("nascimento").build(),
+            User.UserBuilder.builder().setId("1236").setName("Gustabo").setEmail("marquin@nascimento.com").setUsername("gusta").build(),
+            User.UserBuilder.builder().setId("1237").setName("Jefo de morada nova").setEmail("marquin@nascimento.com").setUsername("jefo").build(),
+            User.UserBuilder.builder().setId("1238").setName("Eric rei delas").setEmail("marquin@nascimento.com").setUsername("ericALenda").build(),
 
             )
     }
